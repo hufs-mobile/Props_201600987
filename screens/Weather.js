a8181f8a685d5f006e27f171342da58e56efaeff
simple_29@hufs.ac.kr <simple_29hufs.ac.kr>
2022-12-16 00:50:55 +0900

    Assignment

--- screens/Weather.js
+++ screens/Weather.js
@@ -3,16 +3,50 @@ import { Text, Alert, StyleSheet, View, Image } from "react-native";
 import * as Location from 'expo-location';
 
 import { MaterialCommunityIcons } from '@expo/vector-icons';
-
+import {LinearGradient} from "expo-linear-gradient";
 import {REACT_APP_WEATHER_KEY} from "@env";
 import axios from "axios";
 
 const weatherOptions = {
+    Thunderstorm: {
+        iconName: "weather-lightning",
+        gradient: ["#373B44", "#4286f4"]
+    },
+    Drizzle: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"]
+    },
+    Rain: {
+        iconName: "weather-rainy",
+        gradient: ["#00C6FB", "#005BEA"]
+    },
+    Snow: {
+        iconName: "weather-snowy",
+        gradient: ["#7DE2FC", "#B9B6E5"]
+    },
+    Atmosphere: {
+        iconName: "weather-hail",
+        gradient: ["#89F7FE", "#66A6FF"]
+    },
     Clear: {
-        iconName : "weather-sunny"
+        iconName: "weather-sunny",
+        gradient: ["#FF7300", "#FEF253"]
     },
     Clouds: {
-        iconName : "weather-cloudy"
+        iconName: "weather-cloudy",
+        gradient: ["#D7D2CC", "#304352"]
+    },
+    Mist: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+    },
+    Dust: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
+    },
+    Haze: {
+        iconName: "weather-hail",
+        gradient: ["#4DA0B0", "#D39D38"]
     }
 }
 
@@ -28,17 +62,14 @@ export default class Weather extends React.Component {
                 `https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${REACT_APP_WEATHER_KEY}&units=metric`
             );
             console.log(data);
-            console.log("temperature : " + data.main.temp);
-            console.log("weather " + data.weather[0].main);
-            console.log("icon : " + data.weather[0].icon);
-
-            this.setState({
-                cond : data.weather[0].main,
-                temp : data.main.temp,
-                icon : data.weather[0].icon
-            });
-
-        } catch(error) {
+        console.log(data.main.temp); 
+        console.log(data.main.temp_max);
+        console.log(data.main.temp_min);
+        console.log(data.weather[0].main);
+        this.setState({cond: data.weather[0].main, temp: data.main.temp, temp_max: data.main.temp_max, 
+            temp_min: data.main.temp_min, humidity: data.main.humidity, feels_like: data.main.feels_like});
+        }        
+         catch(error) {
             Alert.alert("Error", error.message)
         }
     };
@@ -66,19 +97,37 @@ export default class Weather extends React.Component {
 
     render () {
 
-        const {cond, temp,icon} = this.state;
+        const {cond, temp, temp_max, temp_min, feels_like, humidity} = this.state;
 
         return (
-            <View style={[styles.container]}>
-              <View style={[styles.halfcontainer]}>
-                <MaterialCommunityIcons name={weatherOptions[cond].iconName} size={24} color="black"/>
-                <Text style={[styles.temptile]}> {temp} </Text>
-              </View>
-            </View>
-        );
 
+            <LinearGradient
+            colors={weatherOptions[cond].gradient}
+            style={styles.container}
+            >
+
+            
+
+            <View style={[styles.container]}>
+                <View style={[styles.halfcontainer]}>
+                    <MaterialCommunityIcons name={weatherOptions[cond].iconName} size={256} color="black" />
+                    <Text style={[styles.temptitle]}> 현재온도:{temp} °C </Text>
+                </View>
+                  <Text style={styles.title}>{cond}</Text>
+                <View style={styles.halfcontainer}>
+
+                  <Text style={styles.temp_max}>최고온도:{temp_max}°C</Text>
+                  <Text style={styles.temp_min}>최저온도:{temp_min}°C</Text>
+                  <Text style={styles.feels_like}>체감온도:{feels_like} °C</Text>
+                  <Text style={styles.humidity}>습도:{humidity}%</Text>
+                </View>
+            </View>
+            </LinearGradient>
+        );
+        
     }
 }
+
 
 const styles = StyleSheet.create({
     container: {
@@ -93,5 +142,17 @@ const styles = StyleSheet.create({
     },
     temptitle: {
         fontSize: 24,
+    },
+    temp_max: {
+        fontSize:25
+    },
+    temp_min:{
+        fontSize:25
+    },
+    feels_like:{
+        fontSize:25
+    },
+    humidity:{
+        fontSize:25
     }
 });
---
+++ screens/YoutubeViewer.js
@@ -0,0 +1,53 @@
+import React from "react";
+import { Button, View, ScrollView, StyleSheet, Text, Image, ImageBackground } from "react-native";
+import Constants from 'expo-constants';
+import { useState, useCallback } from "react";
+import YoutubePlayer from "react-native-youtube-iframe";
+import { TextInput } from "react-native";
+
+
+const YoutubeViewer = (props) => {
+    const [playing,setPlaying] = useState(false);
+    const [playingVideoId, setPlayingVideoId] = useState("tEm9EyEPMYM")
+
+    const onStateChange = useCallback((state) => {
+        if (state === "ended") {
+          setPlaying(false);
+          Alert.alert("video has finished playing!");
+        }
+      }, []);
+    
+      const togglePlaying = useCallback(() => {
+        setPlaying((prev) => !prev);
+      }, []);
+    
+      return (
+        <ScrollView>
+          <YoutubePlayer
+            height={300}
+            play={playing}
+            videoId={playingVideoId}
+            onChangeState={onStateChange}
+          />
+          <Button title={playing ? "pause" : "play"} onPress={togglePlaying} />
+          <TextInput
+              style={styles.input}
+              onChangeText={setPlayingVideoId}
+              value={playingVideoId}
+      />
+        </ScrollView>
+      );
+    }
+
+    const styles = StyleSheet.create({
+        input: {
+          height: 40,
+          margin: 12,
+          borderWidth: 1,
+          padding: 10,
+        },
+      });
+       
+
+
+export default YoutubeViewer;
